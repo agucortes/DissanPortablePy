37515325dbf8cb08c758a3e3d2f12c1b1788ec8e
Agustin Cortes <agucortes@gmail.com>
2024-03-04 15:46:00 -0300

    Change Kivy to Beeware and Toga

---
+++ README.md
@@ -0,0 +1,4 @@
+Android app build in python to connect with Odoo 8.0 api
+This app is intented to be a side companion of sales persons.
+It'll save the producst in a sqlite datebase and allow you to find by code or description.
+You can update the prices when you have a stable internet connection
--- main.py
+++ main.py
@@ -1,33 +1,7 @@
 import datetime
 
-from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
 from controller.dbcontroller import DbController
 from controller.odoocontroller import OdooController
-
-class LoginScreen(Screen):
-
-
-    def on_press_button(self, instance):
-        print(self.usr.text)
-        print(self.pswd.text)
-        self.parent.transition.direction = 'left'
-        self.parent.current = 'products'
-        print('You pressed the button!')
-
-class ProductsScreen(Screen):
-    pass
-
-class DissanApp(App):
-
-
-
-    def build(self):
-        self.sm = ScreenManager()
-        self.sm.add_widget(LoginScreen(name="login"))
-        self.sm.add_widget(ProductsScreen(name='products'))
-
-        return self.sm
 
 
 
@@ -39,7 +13,7 @@ if __name__ == '__main__':
 
    p=odoo.get_all_products()
    db.load_products(p)
-#DissanApp().r
+
 
    #print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
 
